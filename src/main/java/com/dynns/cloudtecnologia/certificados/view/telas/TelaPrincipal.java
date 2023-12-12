bd90c9103e0ab0a12df9a4a636750b90af838c3d
Thiago Melo <thiagojunior@temp.com>
2023-12-12 15:45:35 -0300

    Validando Licença do sistema

--- src/main/java/com/dynns/cloudtecnologia/certificados/view/telas/TelaPrincipal.java
+++ src/main/java/com/dynns/cloudtecnologia/certificados/view/telas/TelaPrincipal.java
@@ -7,6 +7,7 @@ import com.dynns.cloudtecnologia.certificados.model.entity.Certificado;
 import com.dynns.cloudtecnologia.certificados.model.entity.ConfiguracaoCertificado;
 import com.dynns.cloudtecnologia.certificados.utils.DataUtils;
 import com.dynns.cloudtecnologia.certificados.utils.DialogUtils;
+import com.dynns.cloudtecnologia.certificados.utils.LicUtils;
 import com.dynns.cloudtecnologia.certificados.view.table.BotaoDetalhesImpl;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.awt.Color;
@@ -19,37 +20,38 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class TelaPrincipal extends javax.swing.JFrame {
-    
+
     private Dotenv dotenv;
     private InstaladorController instalador;
-    
+
     private CertificadoController certificadoControler;
     private ConfiguracaoCertificadoController configuracaoCertificadoController;
-    
+
     private static final String MSG_SELECIONA_CERTIFICADO = "Favor selecionar o Certificado!";
     private static final String MSG_SENHA_INVALIDA = "A Senha do Certificado não confere com a senha do Instalador!";
     private static final String FILTRO_TODOS = "TODOS";
     private static final String FILTRO_VENCIDOS = "VENCIDOS";
     private static final String FILTRO_VENCER_30_DIAS = "VENCEM_EM_30_DIAS";
-    private static final String FONTE = "Segoe UI";
     private static final Integer INDEX_COLUNA_BTN = 6;
     private static final Integer INDEX_COLUNA_DATA_VENCIMENTO = 2;
     private static final String MSG_PROCESSO_CANCELADO = "Processo cancelado!";
-    
+
     public TelaPrincipal() {
+        LicUtils.validarLic();
         initComponents();
         this.inicializarVariaveis();
         this.configurarExibicaoTela();
         this.processoAutomatico();
+
     }
-    
+
     private void inicializarVariaveis() {
         this.dotenv = Dotenv.configure().load();
         this.instalador = new InstaladorController();
         this.certificadoControler = new CertificadoController();
         this.configuracaoCertificadoController = new ConfiguracaoCertificadoController();
     }
-    
+
     private void configurarExibicaoTela() {
         this.setLocationRelativeTo(null);
         this.setEnabled(true);
@@ -58,32 +60,32 @@ public class TelaPrincipal extends javax.swing.JFrame {
         this.version.setText(dotenv.get("VERSION"));
         this.infoRodape.setText(dotenv.get("INFO_RODAPE"));
     }
-    
+
     private void processoAutomatico() {
         preencherTabelaCertificados();
         new BotaoDetalhesImpl(tabela, INDEX_COLUNA_BTN);
     }
-    
+
     private void preencherTabelaCertificados() {
         tabela.setModel(certificadoControler.preencherTabelaCertificados(new Certificado()));
         definirInformacoesTabela();
     }
-    
+
     private void preencherTabelaCertificadosVencidos() {
         tabela.setModel(certificadoControler.preencherTabelaCertificadosVencidos(new Certificado()));
         definirInformacoesTabela();
     }
-    
+
     private void preencherTabelaCertificadosAtivos() {
         tabela.setModel(certificadoControler.preencherTabelaCertificadosAtivos(new Certificado()));
         definirInformacoesTabela();
     }
-    
+
     private void preencherTabelaCertificadosVencemAte30Dias() {
         tabela.setModel(certificadoControler.preencherTabelaCertificadosVencemAte30Dias(new Certificado()));
         definirInformacoesTabela();
     }
-    
+
     private void preencherTabelaCertificadosPesquisa() {
         String nomePesquisa = nomeCertificado.getText().trim();
         if (nomePesquisa != null && !nomePesquisa.equals("")) {
@@ -93,7 +95,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
             definirInformacoesTabela();
         }
     }
-    
+
     private void definirInformacoesTabela() {
         int totalRegistros = certificadoControler.retornarQtdeRegistros();
         qtdeRegistros.setText("Registros: " + totalRegistros);
@@ -104,7 +106,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
                     + " favor informar ao Responsável! ");
         }
     }
-    
+
     private void definirTamanhoColunas() {
         tabela.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tabela.getColumnModel().getColumn(0).setPreferredWidth(40);//ID 
@@ -115,7 +117,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         tabela.getColumnModel().getColumn(5).setPreferredWidth(250);//DETALHES
         tabela.getColumnModel().getColumn(6).setPreferredWidth(30);//BTN
     }
-    
+
     private void colorirLinhaAtraso() {
         tabela.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
             @Override
@@ -152,7 +154,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
             }
         });
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -385,7 +387,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
             if (isAutenticado()) {
                 int linhaSelecionada = tabela.getSelectedRow();
                 String nomeCert = certificadoControler.retornarNome(linhaSelecionada);
-                
+
                 if (DialogUtils.confirmarOperacao("Confirmar exclusão do certificado? " + nomeCert)) {
                     int idCertificado = certificadoControler.retornarId(linhaSelecionada);
                     certificadoControler.deletarCertificado(idCertificado);
@@ -407,7 +409,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
             String nome = certificadoControler.retornarNome(linhaSelecionada);
             String dataVencimento = certificadoControler.retornarDataVencimentoSTR(linhaSelecionada);
             String alias = certificadoControler.retornarAlias(linhaSelecionada);
-            
+
             if (expira < 0) {
                 JOptionPane.showMessageDialog(null, "Não foi possível instalar o Certificado: \r\n (" + nome + "-" + alias + ")  \r\n "
                         + " CAUSA: Certificado expirado: " + dataVencimento);
@@ -420,7 +422,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 ConfiguracaoCertificado configuracaoCertificado = configuracaoCertificadoController.obterConfiguracaoCertificado();
                 Certificado certificado = certificadoControler.findById(certificadoControler.retornarId(linhaSelecionada));
                 int statusCode = instalador.instalarCertificado(configuracaoCertificado, certificado);
-                
+
                 if (statusCode == 0 && !descricao.contains(MSG_SENHA_INVALIDA)) {
                     JOptionPane.showMessageDialog(null, "Sucesso ao Instalar o certificado: " + nome + ", CÓD: " + statusCode);
                 } else {
@@ -481,18 +483,18 @@ public class TelaPrincipal extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, MSG_SELECIONA_CERTIFICADO);
         }
     }//GEN-LAST:event_btnEnviarCertificadoActionPerformed
-    
+
     private void limpaBarraPesquisa() {
         nomeCertificado.setText("");
     }
-    
+
     private boolean isAutenticado() {
         ConfiguracaoCertificado configuracaoCertificado = configuracaoCertificadoController.obterConfiguracaoCertificado();
         AutenticacaoDialog dialogAutenticacao = new AutenticacaoDialog(this, configuracaoCertificado.getSenhaMaster());
         dialogAutenticacao.setVisible(true);
         return dialogAutenticacao.isAutenticado();
     }
-    
+
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
             new TelaPrincipal().setVisible(true);
